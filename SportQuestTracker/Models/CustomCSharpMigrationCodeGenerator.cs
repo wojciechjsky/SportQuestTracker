ca89936a98025ad281b464b4e76608a700987edb
Patrycja <ppeczynska@gmail.com>
2020-09-08 23:34:27 +0200

    Create a Entity schema

--- SportQuestTracker/Models/CustomCSharpMigrationCodeGenerator.cs
+++ SportQuestTracker/Models/CustomCSharpMigrationCodeGenerator.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Data.Entity.Migrations.Design;
-using System.Text;
+﻿using System.Data.Entity.Migrations.Design;
 
 namespace SportQuestTracker.Models
 {
