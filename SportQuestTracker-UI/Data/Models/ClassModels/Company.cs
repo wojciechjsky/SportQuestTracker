26760a99f3281f82410aa79d3f3e6b63b47d616f
Wojciech Jablonski <jablonski91w@gmail.com>
2020-10-01 22:58:36 +0200

    User login and registration interface without navbar

---
+++ SportQuestTracker-UI/Data/Models/ClassModels/Company.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SportQuestTracker.Models.ClassModels
+{
+    [Table("Companies")]
+    public class Company
+    {
+        [Key]
+        public int CompanyId { get; set; }
+
+        [Required]
+        public string CompanyName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string Email { get; set; }
+
+        public int CollectedCoins { get; set; }
+
+        public string Logo { get; set; }
+
+        public IEnumerable<Gadget> Gadgets { get; set; }
+        public virtual User User { get; set; }
+    }
+}
