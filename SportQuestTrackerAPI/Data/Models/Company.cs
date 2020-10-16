38fbbb0587b3df3293e9eb9100efa170f04e367f
Wojciech Jablonski <jablonski91w@gmail.com>
2020-10-16 18:58:11 +0200

    Setting models

---
+++ SportQuestTrackerAPI/Data/Models/Company.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportQuestTrackerAPI.Data.Models
+{
+    [Table("Companies")]
+    public class Company
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public int Points { get; set; }
+        public List<Gadget> CompanyGadgets { get; set; }
+        public static string Gadget { get; set; }
+        
+    }
+}
---
+++ SportQuestTrackerAPI/Data/Models/User.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportQuestTrackerAPI.Data.Models
+{
+    public class User
+    {
+    }
+}
