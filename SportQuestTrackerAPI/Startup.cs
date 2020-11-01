e594edfe53644a9c798b042339e1cf7e1f58ab2b
patipec <ppeczynska@gmail.com>
2020-11-01 23:56:26 +0100

    Calculator view plus Run calc mock view - needs some fixes

--- SportQuestTrackerAPI/Startup.cs
+++ SportQuestTrackerAPI/Startup.cs
@@ -71,9 +71,9 @@ namespace SportQuestTrackerAPI
                     });
                 c.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
 
-                var xfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-                var xpath = Path.Combine(AppContext.BaseDirectory, xfile);
-                c.IncludeXmlComments(xpath);
+                // var xfile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                // var xpath = Path.Combine(AppContext.BaseDirectory, xfile);
+                // c.IncludeXmlComments(xpath);
 
             });
 
