4e8aa768ae2604429e19bb31f01e0aa004434e96
Wojciech Jablonski <jablonski91w@gmail.com>
2020-11-01 13:55:42 +0100

    Improved views of index page and calculator

--- SportQuestTrackerAPI/Data/SeedData.cs
+++ SportQuestTrackerAPI/Data/SeedData.cs
@@ -88,4 +88,86 @@ namespace SportQuestTrackerAPI.Data
             }
         }
     }
+
+    //public static class SeedData
+    //{
+    //    public static async Task Seed(UserManager<AppUser> userManager, RoleManager<IdentityRole> roleManager)
+    //    {
+    //        await SeedRoles(roleManager);
+    //        await SeedUsers(userManager);
+    //    }
+    //    private static async Task SeedUsers(UserManager<AppUser> userManager)
+    //    {
+    //        if (await userManager.FindByEmailAsync("admin@gmail.com") == null)
+    //        {
+    //            var user = new AppUser
+    //            {
+    //                UserName = "admin@gmail.com",
+    //                Email = "admin@gmail.com",
+    //                FirstName = "Wojtek",
+    //                Surname = "Jablonski",
+    //                Points = 1000,
+    //                Coins = 1000
+    //            };
+    //            var result = await userManager.CreateAsync(user, "Komputer%20");
+    //            if (result.Succeeded)
+    //            {
+    //                await userManager.AddToRoleAsync(user, "Administrator");
+    //            }
+    //        }
+    //        if (await userManager.FindByEmailAsync("pati@gmail.com") == null)
+    //        {
+    //            var user = new AppUser
+    //            {
+    //                UserName = "pati@gmail.com",
+    //                Email = "pati@gmail.com",
+    //                FirstName = "Patrycja",
+    //                Surname = "Peczyńska",
+    //                Points = 1000,
+    //                Coins = 1000
+    //            };
+    //            var result = await userManager.CreateAsync(user, "Komputer%20");
+    //            if (result.Succeeded)
+    //            {
+    //                await userManager.AddToRoleAsync(user, "Customer");
+    //            }
+    //        }
+    //        if (await userManager.FindByEmailAsync("baksiu@gmail.com") == null)
+    //        {
+    //            var user = new AppUser
+    //            {
+    //                UserName = "baksiu@gmail.com",
+    //                Email = "baksiu@gmail.com",
+    //                FirstName = "Baksiu",
+    //                Surname = "Baksiowski",
+    //                Points = 1000,
+    //                Coins = 1000
+    //            };
+    //            var result = await userManager.CreateAsync(user, "Komputer%20");
+    //            if (result.Succeeded)
+    //            {
+    //                await userManager.AddToRoleAsync(user, "Customer");
+    //            }
+    //        }
+    //    }
+    //    private static async Task SeedRoles(RoleManager<IdentityRole> roleManager)
+    //    {
+    //        if (!await roleManager.RoleExistsAsync("Administrator"))
+    //        {
+    //            var role = new IdentityRole
+    //            {
+    //                Name = "Administrator"
+    //            };
+    //            await roleManager.CreateAsync(role);
+    //        }
+    //        if (!await roleManager.RoleExistsAsync("Customer"))
+    //        {
+    //            var role = new IdentityRole
+    //            {
+    //                Name = "Customer"
+    //            };
+    //            await roleManager.CreateAsync(role);
+    //        }
+    //    }
+    //}
 }
