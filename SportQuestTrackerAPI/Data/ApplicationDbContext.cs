114943f1a011661ae2a9fc162b6d5d1e3e310f33
patipec <ppeczynska@gmail.com>
2020-11-03 00:09:03 +0100

    Gadgets, companies and quest seeds, some tries with new navbar

--- SportQuestTrackerAPI/Data/ApplicationDbContext.cs
+++ SportQuestTrackerAPI/Data/ApplicationDbContext.cs
@@ -68,56 +68,106 @@ namespace SportQuestTrackerAPI.Data
                     new Gadget
                     {
                         GadgetId = 1,
-                        Price = 20,
-                        CompanyId = 3,
+                        Price = 22,
+                        CompanyId = 1,
                         Image = "https://cdn.pixabay.com/photo/2020/07/15/18/30/sneakers-5408667__340.png",
                         Name = "Nike shoes",
-                        Quantity = "100",
+                        Quantity = "50",
                         TypeOfProduct = 1,
                     },
                     new Gadget
                     {
                         GadgetId = 2,
-                        Price = 30,
-                        CompanyId = 1,
+                        Price = 34,
+                        CompanyId = 2,
                         Image =
                             "https://media.istockphoto.com/photos/adidas-superstar-picture-id458068097?b=1&k=6&m=458068097&s=170667a&w=0&h=7LAV8cgmrSUtSpqszncqx1Yq5RbVeCR5JzbakOCpkb4=",
                         Name = "Addidas shoes",
-                        Quantity = "100",
+                        Quantity = "50",
                         TypeOfProduct = 1,
                     },
                     new Gadget
                     {
                         GadgetId = 3,
-                        Price = 50,
-                        CompanyId = 2,
-                        Image = "https://cdn.pixabay.com/photo/2018/03/09/10/41/reebok-3211174__340.png",
+                        Price = 44,
+                        CompanyId = 3,
+                        Image = "https://th.bing.com/th/id/OIP.oI3Ua_ewEKpTj4BFaVh-XQHaHa?pid=Api&rs=1",
                         Name = "Reebook Shoes",
-                        Quantity = "100",
+                        Quantity = "50",
                         TypeOfProduct = 1,
                     },
 
                     new Gadget
                     {
                         GadgetId = 4,
-                        Price = 50,
-                        CompanyId = 2,
-                        Image = "https://cdn.pixabay.com/photo/2018/03/09/10/41/reebok-3211174__340.png",
+                        Price = 55,
+                        CompanyId = 3,
+                        Image = "https://th.bing.com/th/id/OIP.c9wNrBxOCc8xJ6nMjNkaOAHaHa?pid=Api&rs=1",
                         Name = "Reebook Shoes",
-                        Quantity = "100",
-                        TypeOfProduct = 1,
+                        Quantity = "30",
+                        TypeOfProduct = 2,
                     },
                     new Gadget
                     {
                         GadgetId = 5,
-                        Price = 50,
-                        CompanyId = 2,
-                        Image = "https://cdn.pixabay.com/photo/2018/03/09/10/41/reebok-3211174__340.png",
-                        Name = "Reebook Shoes",
+                        Price = 22,
+                        CompanyId = 3,
+                        Image = "https://www.tights.no/wp-content/uploads/sites/7/2019/11/0003886_womens-best-paradise-leggings-paradise-green.jpeg",
+                        Name = "Reebook leggins",
                         Quantity = "100",
-                        TypeOfProduct = 1,
+                        TypeOfProduct = 2,
+                    },
+                    
+                    new Gadget
+                    {
+                        GadgetId = 5,
+                        Price = 22,
+                        CompanyId = 3,
+                        Image = "https://www.prodirectrunning.com/ProductImages/Gallery_2/173400.jpg",
+                        Name = "Reebook leggins",
+                        Quantity = "100",
+                        TypeOfProduct = 2,
+                    },
+                    new Gadget
+                    {
+                        GadgetId = 6,
+                        Price = 43,
+                        CompanyId = 5,
+                        Image = "https://th.bing.com/th/id/OIP.kn2UEE6mcOM2Vug_53APgwHaHa?pid=Api&rs=1",
+                        Name = "4F Jacket",
+                        Quantity = "100",
+                        TypeOfProduct = 3,
+                    },
+                    new Gadget
+                    {
+                        GadgetId = 7,
+                        Price = 15,
+                        CompanyId = 3,
+                        Image = "https://i.pinimg.com/originals/86/c7/e9/86c7e947912318d5b788bebcd8715ba9.jpg",
+                        Name = "Reebook Jacket",
+                        Quantity = "100",
+                        TypeOfProduct = 3,
+                    },
+                    new Gadget
+                    {
+                        GadgetId = 8,
+                        Price = 25,
+                        CompanyId = 4,
+                        Image = "https://www.zoltansport.pl/pol_pl_Bluza-meska-Puma-Essentials-TR-czarna-851771-01-30168_1.jpg",
+                        Name = "Puma Jacket",
+                        Quantity = "100",
+                        TypeOfProduct = 3,
+                    },
+                    new Gadget
+                    {
+                        GadgetId = 9,
+                        Price = 30,
+                        CompanyId = 3,
+                        Image = "https://th.bing.com/th/id/OIP.nm2uowz7lsKkEXiPkdItAwHaHa?pid=Api&rs=1",
+                        Name = "Nike Jacket",
+                        Quantity = "100",
+                        TypeOfProduct = 3,
                     }
-
                 );
             //modelBuilder.Entity<Transaction>()
             //    .HasData(
@@ -159,7 +209,7 @@ namespace SportQuestTrackerAPI.Data
                         Id = 2,
                         Name = "Adidas",
                         Password = "111111",
-                        Email = "marketing@adidas.com",
+                        Email = "promo@adidas.com",
                         Points = 1000,
 
                     },
@@ -168,12 +218,30 @@ namespace SportQuestTrackerAPI.Data
                         Id = 3,
                         Name = "Reebook",
                         Password = "111111",
-                        Email = "marketing@reebook.com",
+                        Email = "media@reebook.com",
+                        Points = 1000,
+
+                    },
+                    new Company
+                    {
+                        Id = 4,
+                        Name = "Puma",
+                        Password = "111111",
+                        Email = "contactus@puma.com",
+                        Points = 1000,
+
+                    },
+                    new Company
+                    {
+                        Id = 5,
+                        Name = "4F",
+                        Password = "111111",
+                        Email = "customerservice@4f.com",
                         Points = 1000,
 
                     });
 
-            modelBuilder.Entity<Quest>()
+            modelBuilder.Entity<Quest>() 
                 .HasData(
                     new Quest
                     {
@@ -185,7 +253,7 @@ namespace SportQuestTrackerAPI.Data
                     new Quest
                     {
                         Id = 2,
-                        Name = "Jogging 2km",
+                        Name = "Running 2km",
                         CoinReward = 2,
                         IsDone = false,
                     },
@@ -206,9 +274,58 @@ namespace SportQuestTrackerAPI.Data
                     new Quest
                     {
                         Id = 5,
-                        Name = "Jogging 3km",
+                        Name = "Running 3km",
                         CoinReward = 3,
                         IsDone = false,
+                    },
+                    new Quest
+                    {
+                        Id = 6,
+                        Name = "Weightlifting 30 minutes",
+                        CoinReward = 5,
+                        IsDone = false,
+                    },
+                    new Quest
+                    {
+                        Id = 7,
+                        Name = "Running 5km",
+                        CoinReward = 6,
+                        IsDone = false,
+                    },
+                    new Quest
+                    {
+                        Id = 8,
+                        Name = "Cardio 30 minutes",
+                        CoinReward = 2,
+                        IsDone = false,
+                    },
+                    new Quest
+                    {
+                        Id = 9,
+                        Name = "Cardio 60 minutes",
+                        CoinReward = 4,
+                        IsDone = false,
+                    },
+                    new Quest
+                    {
+                        Id = 10,
+                        Name = "Cycling 10 km",
+                        CoinReward = 3,
+                        IsDone = false
+                    },
+                    new Quest
+                    {
+                        Id = 11,
+                        Name = "Cycling 20 km",
+                        CoinReward = 5,
+                        IsDone = false
+                    },
+                    new Quest
+                    {
+                        Id = 12,
+                        Name = "Cycling 30 km",
+                        CoinReward = 7,
+                        IsDone = false
                     }
 
                 );
