77ab355ab248b0375b6e9986b1008b600bf3aba0
Wojciech Jablonski <jablonski91w@gmail.com>
2020-11-07 11:01:01 +0100

    UnitTesting

---
+++ SportQuestTrackerUnitTest/GadgetControllerTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using SportQuestTrackerAPI.Controllers;
+
+namespace SportQuestTrackerUnitTest
+{
+    public class GadgetControllerTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        //public async Task<IActionResult> GetGadget(int id)
+        [Test]
+        public void GetGadget_WhenGadgetNotExist_ReturnsNotFound()
+        {
+            var GadgetController = new GadgetController();
+        }
+
+
+    }
+}
